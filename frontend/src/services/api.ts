e63527c0e34509f9b1f0ee04c2a6defbc74e335c
RainenterMudin <rianikhamudin152@gmail.com>
2026-08-13 22:41:18 +0700

    Fix backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://catalog-14qq.onrender.com/api',
+  baseURL: 'https://catalog-14qg.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
